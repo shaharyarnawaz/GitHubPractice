54036a02e62935b20ce73806ac30f36f6fa11446
Shaharyar <shaharyarkeerio@gmail.com>
2017-10-16 12:35:03 +0500

    Modified MainActivity Added new File " TestAddFile.java "

--- app/src/main/java/com/alts/shery/githubpractice/MainActivity.java
+++ app/src/main/java/com/alts/shery/githubpractice/MainActivity.java
@@ -10,4 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void testingGit()
+    {
+        int i=1;
+        int ii=i+2;
+        int iii=ii+1;
+
+
+    }
 }
---
+++ app/src/main/java/com/alts/shery/githubpractice/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.alts.shery.githubpractice;
+
+/**
+ * Created by SHERY on 10/16/2017.
+ */
+
+public class TestAddFile {
+}
